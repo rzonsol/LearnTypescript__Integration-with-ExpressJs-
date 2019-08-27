dc7f1356fa19e630e8650562e40b3f048273205f
przonsow <piotr.rzonsowski@cognitran.com>
2019-08-27 09:36:50 +0200

    Add login endpoint

--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 
 const router = Router();
 
@@ -8,6 +8,28 @@ router.get('/', (req, res) => {
             <h1>Hi there</h1>
         </div>
     `);
+});
+
+router.get('/login', (req: Request, res: Response) => {
+	res.send(`
+    <form method="POST">
+        <div>
+            <label for="">Email</label>
+            <input type="text" name="email" />
+        </div>
+        <div>
+            <label for="">Password</label>
+            <input type="password" name="password" />
+        </div>
+        <button>Submit</button>
+    </form>
+    `);
+});
+
+router.post('/login', (req: Request, res: Response) => {
+	const { email, password } = req.body;
+
+	res.send(email + password);
 });
 
 export { router };
