ef1cae373b6ae3a0fd479696ba81546b970467d0
przonsow <piotr.rzonsowski@cognitran.com>
2019-08-27 10:17:43 +0200

    add interface to overide response class

--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -1,5 +1,9 @@
 import { Router, Request, Response } from 'express';
 
+interface RequestWithBody extends Request {
+	body: { [key: string]: string | undefined };
+}
+
 const router = Router();
 
 router.get('/', (req, res) => {
@@ -10,7 +14,7 @@ router.get('/', (req, res) => {
     `);
 });
 
-router.get('/login', (req: Request, res: Response) => {
+router.get('/login', (req: RequestWithBody, res: Response) => {
 	res.send(`
     <form method="POST">
         <div>
@@ -26,10 +30,14 @@ router.get('/login', (req: Request, res: Response) => {
     `);
 });
 
-router.post('/login', (req: Request, res: Response) => {
+router.post('/login', (req: RequestWithBody, res: Response) => {
 	const { email, password } = req.body;
 
-	res.send(email + password);
+	if (email) {
+		res.send(email.toUpperCase());
+	} else {
+		res.send('Email requ');
+	}
 });
 
 export { router };
